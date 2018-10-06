89d3a87fff6be02e91859b9a2bf7912d4a7976da
pdog <pdog@qq.com>
2018-10-06 18:04:01 +0800

    set default launch mode plugin

--- buildSrc/src/main/java/plugin/pdog/com/buildsrc/SetDefaultLaunchModePlugin.kt
+++ buildSrc/src/main/java/plugin/pdog/com/buildsrc/SetDefaultLaunchModePlugin.kt
@@ -2,39 +2,27 @@ package plugin.pdog.com.buildsrc
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.tasks.MergeManifests
 import groovy.util.XmlSlurper
 import groovy.util.slurpersupport.NodeChild
-import groovy.util.slurpersupport.NodeChildren
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import java.io.File
 
-class PluginBuildSrc : Plugin<Project> {
+class SetDefaultLaunchModePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.plugins.all {
-            when (it) {
-                is AppPlugin -> {
-                    project.extensions.getByType(AppExtension::class.java).run {
-                        findProcessManifest(project, this)
-                    }
-                }
+            if (it !is AppPlugin) {
+                return@all
             }
-        }
-    }
 
-    private fun findProcessManifest(project: Project, appExtension: AppExtension) {
-        appExtension.applicationVariants.all { variant ->
-            variant.outputs.forEach {
-                val mergeManifests = it.processManifest as MergeManifests
-                mergeManifests.doFirst {
-                    val allEmptyLaunchModeActivityFiles = findAllEmptyLaunchModeActivityFiles(it.inputs.files)
-                    allEmptyLaunchModeActivityFiles.forEach {
-                        println(it.key)
-                        it.value.forEach {
-                            val nodeName = it.attributes()["{http://schemas.android.com/apk/res/android}name"]
-                            println(nodeName)
+            project.extensions.getByType(AppExtension::class.java).run {
+                this.applicationVariants.all { variant ->
+                    variant.outputs.forEach {
+
+                        val task = it.processManifest
+                        task.doLast {
+                            find(task.manifestOutputDirectory)
                         }
                     }
                 }
@@ -42,30 +30,29 @@ class PluginBuildSrc : Plugin<Project> {
         }
     }
 
-    private fun findAllEmptyLaunchModeActivityFiles(file: FileCollection): Map<File, List<NodeChild>> {
-        val map = hashMapOf<File, List<NodeChild>>()
-        file.forEach {
-            if (it.isDirectory) {
-                return@forEach
-            }
-
-            val emptyLaunchModeActivitiesOnFile = findActivityFromManifestFile(it)
-            if (emptyLaunchModeActivitiesOnFile.isNotEmpty()) {
-                map[it] = emptyLaunchModeActivitiesOnFile
+    private fun find(file: File) {
+        if (file.isDirectory) {
+            file.listFiles().forEach { find(it) }
+        } else {
+            if (file.name == "AndroidManifest.xml") {
+                findActivityFromManifestFile(file)
+            } else {
+                println("other file ${file.name}")
             }
         }
-        return map
     }
+
 
     private fun findActivityFromManifestFile(file: File): List<NodeChild> {
         val emptyLaunchModeActivities = arrayListOf<NodeChild>()
 
         val androidManifest = XmlSlurper().parse(file)
-        val application = androidManifest.children().getAt(0) as NodeChild
+        val application = androidManifest.children().getAt(1) as NodeChild
 
         val children = application.children()
         val childrenCount = children.size()
         if (childrenCount <= 0) {
+            println("childrenCount <= 0")
             return emptyLaunchModeActivities
         }
         println("-------findActivityFromManifestFile start--------")
@@ -84,6 +71,8 @@ class PluginBuildSrc : Plugin<Project> {
                 println("$activityName  not have any launch mode")
                 emptyLaunchModeActivities.add(component)
             }
+
+            println("activityName ${activityName}  launchMode ${launchMode}")
         }
 
         println("-------findActivityFromManifestFile end--------")
