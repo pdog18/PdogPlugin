ec5e4494f223e56eca2304dc1171bb829b2abf89
pdog <pdog@qq.com>
2018-10-07 18:49:46 +0800

    use extensions for plugin

---
+++ buildSrc/src/main/java/plugin/pdog/com/buildsrc/ActivityAttributes.kt
@@ -0,0 +1,9 @@
+package plugin.pdog.com.buildsrc
+
+open class ActivityAttributes {
+    var screenOrientation: String? = null
+    var launchMode: String? = null
+    var windowSoftInputMode: String? = null
+    var configChanges: String? = null
+//    val theme: String? = null
+}
