861a4928644be2b58f555f79ad1a1c4849796f5b
pdog <pdog@qq.com>
2018-10-10 11:34:25 +0800

    create branch merge-module

--- sample/src/main/java/com/pdog18/plugin/BaseActivity.kt
+++ sample/src/main/java/com/pdog18/plugin/BaseActivity.kt
@@ -4,9 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.pdog.attributes.KeepAttributes
 
-@KeepAttributes("BaseActivityKeep")
 abstract class BaseActivity : AppCompatActivity() {
     final override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- sample/src/main/java/com/pdog18/plugin/EmptyScreenOrientationActivity.kt
+++ sample/src/main/java/com/pdog18/plugin/EmptyScreenOrientationActivity.kt
@@ -1,6 +1,5 @@
 package com.pdog18.plugin
 
-import com.pdog.attributes.KeepAttributes
 import kotlinx.android.synthetic.main.activity_base.*
 
 class EmptyScreenOrientationActivity : BaseActivity() {
